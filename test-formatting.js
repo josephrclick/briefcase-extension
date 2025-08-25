0394c75836f095fd9cfddd5bda03e4d2afcb1775
Joseph Click <josephrclick@gmail.com>
2025-08-24 22:21:28 -0700

    test: successfully tested pre-commit hooks with malformed file

---
+++ test-formatting.js
@@ -0,0 +1,13 @@
+// Intentionally malformed JavaScript for testing
+const test = {
+  foo: "bar",
+  baz: "qux",
+};
+
+function badFormatting() {
+  console.log("test");
+}
+
+const unusedVar = 42;
+
+export { badFormatting };
